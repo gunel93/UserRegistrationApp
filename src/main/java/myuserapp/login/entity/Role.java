45954ca19f316cb99a35cb206869d1dd1ce82b22
gunel93 <s339490@oslomet.no>
2022-01-16 15:27:45 +0100

    Initial commit

---
+++ src/main/java/myuserapp/login/entity/Role.java
@@ -0,0 +1,21 @@
+package myuserapp.login.entity;
+
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Setter
+@Getter
+@Entity
+@Table(name = "roles")
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+
+
+}
---
+++ src/main/java/myuserapp/login/repository/RoleRepository.java
@@ -0,0 +1,10 @@
+package myuserapp.login.repository;
+
+import myuserapp.login.entity.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface RoleRepository extends JpaRepository<Role, Long>{
+    Optional<Role> findByName(String name);
+}
